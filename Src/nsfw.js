bf6c362708c91abab0a65587cc834ea7eb6f7fd0
Diwas Atreya <74746579+diwasatreya@users.noreply.github.com>
2022-05-27 20:42:15 +0545

    Update nsfw.js

--- Src/nsfw.js
+++ Src/nsfw.js
@@ -438,6 +438,220 @@ return Data;
 };
 
 
+/**
+ * Return A Bikini nsfw Image
+ * @returns {Data}
+ */
+
+async function Bikini(options = {}) {
+ var subreddits = [
+          'bikinis',
+          'bikinibodies'
+        ]
+
+var reddit = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+      
+        const data = await fetch(`https://meme-api.herokuapp.com/gimme/${reddit}`).then(res => res.json())
+
+        if (!data) return message.channel.send(`Sorry, seems like i can't connect to API.`);
+      
+        const { title, postLink, url, subreddit } = data
+
+const Data = {
+        embed: {
+            color: options.Color || "RANDOM",
+            url: postLink,
+            title: title,
+            image: { url: url },
+            footer: { text: `r/${subreddit}` }
+        }
+    }
+
+    if (options.ResultOnly && options.ResultOnly === true) return {
+        Result: {
+            Url: postLink,
+            Title: title,
+            Image: url,
+            Subreddit: `${subreddit}`
+        }
+
+
+}
+return Data;
+};
+
+/**
+ * Return A Nsfw Socks Image
+ * @returns {Data}
+ */
+
+async function Socks(options = {}) {
+  var subreddits = [
+          'GirlsInSocks',
+          'girlsinanklesocks'
+        ]
+var reddit = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+      
+        const data = await fetch(`https://meme-api.herokuapp.com/gimme/${reddit}`).then(res => res.json())
+
+        if (!data) return message.channel.send(`Sorry, seems like i can't connect to API.`);
+      
+        const { title, postLink, url, subreddit } = data
+
+const Data = {
+        embed: {
+            color: options.Color || "RANDOM",
+            url: postLink,
+            title: title,
+            image: { url: url },
+            footer: { text: `r/${subreddit}` }
+        }
+    }
+
+    if (options.ResultOnly && options.ResultOnly === true) return {
+        Result: {
+            Url: postLink,
+            Title: title,
+            Image: url,
+            Subreddit: `${subreddit}`
+        }
+
+
+}
+return Data;
+};
+
+/**
+ * Return A Nsfw Tattoo Image
+ * @returns {Data}
+ */
+
+
+async function Tattoo(options = {}) {
+   var subreddits = [
+          'HotChicksWithTattoos',
+          'SuicideGirls',
+          'SceneGirls',
+          'PrettyAltGirls'
+        ]
+
+var reddit = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+      
+        const data = await fetch(`https://meme-api.herokuapp.com/gimme/${reddit}`).then(res => res.json())
+
+        if (!data) return message.channel.send(`Sorry, seems like i can't connect to API.`);
+      
+        const { title, postLink, url, subreddit } = data
+
+const Data = {
+        embed: {
+            color: options.Color || "RANDOM",
+            url: postLink,
+            title: title,
+            image: { url: url },
+            footer: { text: `r/${subreddit}` }
+        }
+    }
+
+    if (options.ResultOnly && options.ResultOnly === true) return {
+        Result: {
+            Url: postLink,
+            Title: title,
+            Image: url,
+            Subreddit: `${subreddit}`
+        }
+
+
+}
+return Data;
+};
+
+/**
+ * Return A Thighs Image
+ * @returns {Data}
+ */
+
+async function Thighs(options = {}) {
+     var subreddits = [
+          'thighs',
+          'PerfectThighs',
+          'thickthighs'
+        ]
+
+var reddit = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+      
+        const data = await fetch(`https://meme-api.herokuapp.com/gimme/${reddit}`).then(res => res.json())
+
+        if (!data) return message.channel.send(`Sorry, seems like i can't connect to API.`);
+      
+        const { title, postLink, url, subreddit } = data
+
+const Data = {
+        embed: {
+            color: options.Color || "RANDOM",
+            url: postLink,
+            title: title,
+            image: { url: url },
+            footer: { text: `r/${subreddit}` }
+        }
+    }
+
+    if (options.ResultOnly && options.ResultOnly === true) return {
+        Result: {
+            Url: postLink,
+            Title: title,
+            Image: url,
+            Subreddit: `${subreddit}`
+        }
+
+
+}
+return Data;
+};
+
+/**
+ * Return A Nsfw Yogapants Image
+ * @returns {Data}
+ */
+
+async function Yogapants(options = {}) {
+    var subreddits = [
+          'girlsinyogapants',
+          'YogaPants',
+        ]
+
+var reddit = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+      
+        const data = await fetch(`https://meme-api.herokuapp.com/gimme/${reddit}`).then(res => res.json())
+
+        if (!data) return message.channel.send(`Sorry, seems like i can't connect to API.`);
+      
+        const { title, postLink, url, subreddit } = data
+
+const Data = {
+        embed: {
+            color: options.Color || "RANDOM",
+            url: postLink,
+            title: title,
+            image: { url: url },
+            footer: { text: `r/${subreddit}` }
+        }
+    }
+
+    if (options.ResultOnly && options.ResultOnly === true) return {
+        Result: {
+            Url: postLink,
+            Title: title,
+            Image: url,
+            Subreddit: `${subreddit}`
+        }
+
+
+}
+return Data;
+};
+
+
 module.exports = {
-    Ass, Boobs, Pussy, Teen, Feet, Hentai, Nsfw, Cum
+    Ass, Boobs, Pussy, Teen, Feet, Hentai, Nsfw, Cum, Bikini, Socks, Tattoo, Thighs, Yogapants
 };
