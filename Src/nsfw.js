cb0fc9b4c87464fac3c5fd8013c9480056e6e6c6
Diwas Atreya <74746579+diwasatreya@users.noreply.github.com>
2022-05-27 19:58:11 +0545

    Update nsfw.js

--- Src/nsfw.js
+++ Src/nsfw.js
@@ -1,5 +1,10 @@
 const fetch = require("node-fetch");
 
+/**
+ * Return A Ass Image
+ * @returns {Data}
+ */
+
 async function Ass(options = {}) {
   var subreddits = [
           'asstastic',
@@ -29,7 +34,7 @@ const Data = {
             url: postLink,
             title: title,
             image: { url: url },
-            footer: { text: `r/${subreddit} Powered By: Supreme Club` }
+            footer: { text: `r/${subreddit}` }
         }
     }
 
@@ -38,7 +43,56 @@ const Data = {
             Url: postLink,
             Title: title,
             Image: url,
-            Subreddit: `${subreddit} Powered By: Supreme Club`
+            Subreddit: `${subreddit}`
+        }
+
+
+}
+return Data;
+};
+
+
+/**
+ * Return A Boobs Image
+ * @returns {Data}
+ */
+
+async function Boobs(options = {}) {
+  var subreddits = [
+          'boobs',
+          'Boobies',
+          'Stacked',
+          'BustyPetite',
+          'Cleavage',
+          'bustyasians',
+          'boltedontits',
+          'burstingout'
+        ]
+
+var reddit = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+      
+        const data = await fetch(`https://meme-api.herokuapp.com/gimme/${reddit}`).then(res => res.json())
+
+        if (!data) return message.channel.send(`Sorry, seems like i can't connect to API.`);
+      
+        const { title, postLink, url, subreddit } = data
+
+const Data = {
+        embed: {
+            color: options.Color || "RANDOM",
+            url: postLink,
+            title: title,
+            image: { url: url },
+            footer: { text: `r/${subreddit}` }
+        }
+    }
+
+    if (options.ResultOnly && options.ResultOnly === true) return {
+        Result: {
+            Url: postLink,
+            Title: title,
+            Image: url,
+            Subreddit: `${subreddit}`
         }
 
 
@@ -48,5 +102,5 @@ return Data;
 
 
 module.exports = {
-    Ass
+    Ass, Boobs
 };
